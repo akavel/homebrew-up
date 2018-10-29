a54b875f45751e7d2018efd91d06c85a7fb9b98e
goreleaserbot <goreleaser@carlosbecker.com>
2018-10-29 01:10:37 +0100

    Brew formula update for  version v0.3

---
+++ .rb
@@ -0,0 +1,11 @@
+class  < Formula
+  desc "Ultimate Plumber is a tool for writing pipes with instant live preview"
+  homepage "https://github.com/akavel/up"
+  url "https://github.com///releases/download/v0.3/_0.3_darwin_amd64.tar.gz"
+  version "0.3"
+  sha256 "3a5483698b6a0e518860f2a2c47f0093593bf18d3551f5d89df0811da190750d"
+
+  def install
+    bin.install ""
+  end
+end
